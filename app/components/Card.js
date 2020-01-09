94dbc22f46afb6249300b5540bcb8f7f2475ad98
sirStephen <itobitoast@gmail.com>
2020-01-10 00:48:27 +0100

    fetch planet names

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './Card';
+import './Card.css';
+import { FaUser } from 'react-icons/fa';
+import { MdEmail } from "react-icons/md";
+import { TiWorld } from "react-icons/ti";
 
-const Card = ({ id, name, email }) => {
+const Card = ({ id, name, email, planetname }) => {
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 card-width'>
             <img 
                 src={`https://robohash.org/${id}?size=200x200`}
                 alt="robots"/>
-            <div>
-                <p className='robot-name'>{name}</p>
-                <p>{email}</p>
+            <div className='tl'>
+                <p className='robot-name flex'><span className='pr'><FaUser /></span>{name}</p>
+                <p className='flex'><MdEmail className='pr'/>{email}</p>
+                <p className='flex'><TiWorld className='pr'/>{planetname}</p>
             </div>
         </div>
     );
--- app/components/CardList.js
+++ app/components/CardList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Card from './Card';
 
-const CardList = ({ robots }) => {
-    
+const CardList = ({ robots, planets }) => {
+
     const cardComponent = robots.map((robot, i) => {
         return (
             <Card 
                 key={robots[i].id} 
                 id={robots[i].id} 
                 name={robots[i].name} 
-                email={robots[i].email} 
+                email={robots[i].email}
+                planetname={planets[i].name}
             />
         );
     });
